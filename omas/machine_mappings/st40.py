573d0f992f1017ee48cce8092d94bece59f62653
Peter Buxton <pfbuxton@users.noreply.github.com>
2022-04-26 10:07:23 +0100

    added more equilibrium keys

--- omas/machine_mappings/st40.py
+++ omas/machine_mappings/st40.py
@@ -36,15 +36,40 @@ def equilibrium__tokamak_energy(ods, pulse, code_run):
     # Time
     ods['equilibrium.time'] = equilibrium_results.get('TIME')
 
+    ## Time slices
     for iTime in range(len(equilibrium_results.get('TIME'))):
 
-        ## "GLOBAL" variables
+        ## Time
         ods['equilibrium.time_slice'][iTime]['time'] = equilibrium_results.get('TIME')[iTime]
+
+        ## "GLOBAL" variables
+        ods['equilibrium.time_slice'][iTime]['global_quantities.beta_normal'] = equilibrium_results.get('GLOBAL.BETAN')[iTime]
+        ods['equilibrium.time_slice'][iTime]['global_quantities.beta_tor'] = equilibrium_results.get('GLOBAL.BETAT')[iTime]
+        
+        ods['equilibrium.time_slice'][iTime]['global_quantities.current_centre.r'] = equilibrium_results.get('GLOBAL.RIP')[iTime] / equilibrium_results.get('GLOBAL.IP')[iTime]
+        ods['equilibrium.time_slice'][iTime]['global_quantities.current_centre.z'] = equilibrium_results.get('GLOBAL.ZIP')[iTime] / equilibrium_results.get('GLOBAL.IP')[iTime]
+        
+        ods['equilibrium.time_slice'][iTime]['global_quantities.energy_mhd'] = equilibrium_results.get('GLOBAL.WMHD')[iTime]
         ods['equilibrium.time_slice'][iTime]['global_quantities.ip'] = equilibrium_results.get('GLOBAL.IP')[iTime]
         ods['equilibrium.time_slice'][iTime]['global_quantities.li_3'] = equilibrium_results.get('GLOBAL.LI3')[iTime]
+        ods['equilibrium.time_slice'][iTime]['global_quantities.magnetic_axis.r'] = equilibrium_results.get('GLOBAL.RMAG')[iTime]
+        ods['equilibrium.time_slice'][iTime]['global_quantities.magnetic_axis.z'] = equilibrium_results.get('GLOBAL.ZMAG')[iTime]
+        
+        ods['equilibrium.time_slice'][iTime]['global_quantities.psi_axis'] = equilibrium_results.get('GLOBAL.PSIA')[iTime]
+        ods['equilibrium.time_slice'][iTime]['global_quantities.psi_boundary'] = equilibrium_results.get('GLOBAL.PSIB')[iTime]
+        
+        ods['equilibrium.time_slice'][iTime]['global_quantities.q_95'] = equilibrium_results.get('GLOBAL.Q95')[iTime]
         ods['equilibrium.time_slice'][iTime]['global_quantities.q_axis'] = equilibrium_results.get('GLOBAL.Q0')[iTime]
         ods['equilibrium.time_slice'][iTime]['global_quantities.volume'] = equilibrium_results.get('GLOBAL.VPLASMA')[iTime]
-        ods['equilibrium.time_slice'][iTime]['global_quantities.energy_mhd'] = equilibrium_results.get('GLOBAL.WMHD')[iTime]
+
+        ## Profiles
+        ods['equilibrium.time_slice'][iTime]['profiles_1d.dpressure_dpsi'] = equilibrium_results.get('PROFILES.PPRIME')[iTime, :]
+        ods['equilibrium.time_slice'][iTime]['profiles_1d.f_df_dpsi'] = equilibrium_results.get('PROFILES.FFPRIME')[iTime, :]
+
+        ods['equilibrium.time_slice'][iTime]['profiles_1d.psi'] = equilibrium_results.get('PROFILES.PSI')[iTime, :]
+
+        ods['equilibrium.time_slice'][iTime]['profiles_1d.rho_tor'] = equilibrium_results.get('PROFILES.RHOTOR')[iTime, :]
+
 
         ## "PSI2D" variables
         # Grid
@@ -61,6 +86,10 @@ def equilibrium__tokamak_energy(ods, pulse, code_run):
         ods['equilibrium.time_slice'][iTime]['profiles_2d[0].b_field_r'] = equilibrium_results.get('PSI2D.FREE.BR')[iTime, :, :]
         ods['equilibrium.time_slice'][iTime]['profiles_2d[0].b_field_z'] = equilibrium_results.get('PSI2D.FREE.BZ')[iTime, :, :]
         ods['equilibrium.time_slice'][iTime]['profiles_2d[0].b_field_tor'] = equilibrium_results.get('PSI2D.FREE.BPHI')[iTime, :, :]
+
+        ## Vacuum toroidal field
+        ods['equilibrium.vacuum_toroidal_field.b0'] = equilibrium_results.get('GLOBAL.BT_VAC_AT_R0P4')
+        ods['equilibrium.vacuum_toroidal_field.r0'] = 0.4
 
 
 # ================================
